7b2b5be83ee39c6cd975de2e9c6e706a39046ba7
Stéphane Le Solliec <stephane@metacites.net>
2018-03-29 23:52:23 +0200

    Reading house state from json file. Tuning UI. Adding colors.

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  var rooms = {
-      'Kitchen': {
-          name: 'Cuisine',
-          temp: 18.0,
-          heater: 'off',
-          wanted: 17
-      },
-      'Living': {
-          name: 'Salle',
-          temp: 18.0,
-          heater: 'off',
-          wanted: 17
-      },
-      'Bathroom': {
-          name: 'SdBain',
-          temp: 18.0,
-          heater: 'off',
-          wanted: 17
-      },
-      'Bedroom': {
-      name: 'Chambre',
-      temp: 15.6,
-      heater: 'on',
-      wanted: 17
-      }
-  };
+    // we get the state of the House
+    var state = JSON.parse( fs.readFileSync( path.join(__dirname, '..' ,'data', 'house-state.json')));
 
-
-  res.render('index', { title: 'Express', how: 'cool', rooms: rooms });
+    res.render('index', { title: 'House state', rooms: state.rooms });
 });
 
 module.exports = router;
