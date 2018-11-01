5d5684df3595127f49c2f88cdbf92ac0757960a1
Stéphane Le Solliec <slesolliec@gmail.com>
2018-11-01 18:21:05 +0100

    better display and implementing temperature modifier

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,18 @@ router.get("/", function(req, res, next) {
 router.get("/getState", function(req, res, next) {
 	res.setHeader("Content-Type", "application/json");
 	res.send( JSON.stringify(req.app.locals.domoticz.state, null, "	") );
+});
 
+router.get('/setTempModifier', function(req, res, next) {
+	console.log("modifying temp of room " + req.query.room + ": " + req.query.tempModifier + " degrees for " + req.query.duration + " hours");
+	req.app.locals.domoticz.state.rooms[req.query.room].tempModifier = parseInt(req.query.tempModifier);
+	var now = new Date();
+	now.setMinutes(now.getMinutes() + req.query.duration * 60);
+	req.app.locals.domoticz.state.rooms[req.query.room].tempModifierUntil = now;
+	console.log(req.app.locals.domoticz.state);
+
+	res.setHeader("Content-Type", "application/json");
+	res.send( JSON.stringify(req.app.locals.domoticz.state, null, "	") );
 });
 
 module.exports = router;
